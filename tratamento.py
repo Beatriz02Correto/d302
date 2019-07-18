0f6cf5117f37e64d1b5970bb388dd98aa2d0abe7
Beatriz <bccp02@gmail.com>
2019-07-18 11:35:18 -0300

    Finalizar

---
+++ tratamento.py
@@ -0,0 +1,22 @@
+# --------------------------------------------------------------------------------------PARTE 1
+
+# numero = None
+
+# while numero is None:
+#     try:
+#         numero = int(input('digite um numero inteiro: '))
+#     except ValueError:
+#         print("erro: era para ter digitado um numero inteiro")
+# # print(f'o numero digitado foi {numero}')
+# --------------------------------------------------------------------------------------PARTE 2
+# numero = None
+
+# while numero is None:
+#     try:
+#         numero = int(input('digite um numero inteiro: '))
+#     except Exception: #exception diz: QUE PARA TODOS OS ERROS QUE DER, DIZER ESSAS FRASES, MAS NUNCA USAR, POIS ABREPORTA PARA BAG NO CODIGO
+#         print("erro: era para ter digitado um numero inteiro")
+# print(f'o numero digitado foi {numero}')
+# --------------------------------------------------------------------------------------PARTE3
+
+def so (*args)
